39c0bacf5e26132c46899f7c125b6ce691387171
Nathan Schucher <nathan.schucher@gmail.com>
2016-11-02 23:41:53 -0400

    Remove drop target from Link component

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,34 +1,9 @@
 import React from 'react';
-import { DropTarget } from 'react-dnd'
-
-const ItemTypes = {
-  LAYER: 'layer'
-}
-
-const linkTarget = {
-  hover(props, monitor, component) {
-    console.log("link hover")
-  },
-  drop(props, monitor, component) {
-    const isJustOverThisOne = monitor.isOver({ shallow: true });
-    if(isJustOverThisOne) {
-      props.onLayerAdd(monitor.getItem(), props)
-    }
-  }
-}
-
-const collect = (connect, monitor) => {
-  return {
-    connectDropTarget: connect.dropTarget(),
-    isOver: monitor.isOver()
-  }
-}
 
 const Link = props => {
-  const { connectDropTarget, isOver, ...rest } = props
-  return connectDropTarget(<line { ...rest }
+  return (<line { ...props }
     strokeWidth="2" stroke="black"
   />)
 }
 
-export default DropTarget(ItemTypes.LAYER, linkTarget, collect)(Link)
+export default Link
